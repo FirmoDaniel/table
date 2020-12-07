a660acd5abb2cdecba9f9a23a096ff70f98f3076
FirmoDaniel <firmodaniel@gmail.com>
2020-12-07 12:42:14 +0000

    initial commit

---
+++ js/my_script.js
@@ -0,0 +1,13 @@
+/*$(document).ready(function(){
+    $("h2").addClass("underline"); //underline all h2 elements
+    $("ul").addClass("border"); // adds a border to all lists
+});*/
+
+/*$(document).ready(function(){
+    $("h2").removeClass("underline"); //underline all h2 elements
+    $("ul").removeClass("border"); // adds a border to all lists
+});*/
+
+$(document).ready(function(){
+
+});
